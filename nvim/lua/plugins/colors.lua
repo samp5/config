9399b30e37189ea8ef6929dac4ef64c9a4eb0836
Sam Praneis <sampraneis@gmail.com>
2023-12-14 10:39:46 -0600

    colors

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -15,12 +15,24 @@ return {
 					-- add/modify theme and palette colors
 					palette = {
 						-- change background color
---						sumiInk3 = "#1F1F28",
+						 sumiInk6 = "#54546D",
 						-- change line number, etc color
 --						sumiInk4 = "#121212",
 					},
 					theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
 				},
+				overrides = function(colors)
+					local theme = colors.theme
+					return {
+						TelescopeTitle = { fg = theme.ui.special, bold = true },
+						TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+						TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+						TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+						TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+						TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+						TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+						}
+				end,
 				theme = "wave", -- Load "wave" theme when 'background' option is not set
 				background = {
 					-- map the value of 'background' option to a theme
@@ -84,15 +96,6 @@ return {
 			vim.api.nvim_set_keymap("n", "<leader>xx", "<cmd>Trouble<cr>",
 				{ silent = true, noremap = true }
 			)
-		end
-	},
-	{
-		'sindrets/diffview.nvim',
-		dependencies = 'nvim-lua/plenary.nvim',
-		config = function()
-			require("diffview").setup({
-				enhanced_diff_hl = true
-			})
 		end
 	},
 }
