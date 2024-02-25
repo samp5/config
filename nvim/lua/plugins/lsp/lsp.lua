ff69b8325d4739cecaf7effb27e24c0b732f8309
Sam Praneis <sampraneis@gmail.com>
2024-02-24 21:02:02 -0600

    fixed lsp file

--- nvim/lua/plugins/lsp/lsp.lua
+++ nvim/lua/plugins/lsp/lsp.lua
@@ -40,6 +40,31 @@ return {
         bnnoremap('gd', '<cmd>lua require("telescope.builtin").lsp_definitions()<cr>', 'go defintion', bufnr)
       end
 
+      local clang_attach = function(client, bufnr)
+        require("lsp-format").on_attach(client, bufnr)
+        require 'nvim-navic'.attach(client, bufnr)
+
+        -- clangd_extensions tools
+        require("clangd_extensions.inlay_hints").setup_autocmd()
+        require("clangd_extensions.inlay_hints").set_inlay_hints()
+
+        -- regular buffer maps
+        bnnoremap('<leader>i', '<cmd>lua vim.lsp.buf.signature_help()<CR>', 'Signature help', bufnr)
+        bnnoremap('<leader>tD', '<cmd>lua vim.lsp.buf.type_definition()<CR>', 'Type definition', bufnr)
+        bnnoremap('gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', 'Go to declaration', bufnr)
+        bnnoremap('gi', '<cmd>Telescope lsp_implementations()<CR>', 'Telescope implementation', bufnr)
+        bnnoremap('<leader>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', 'See code actions', bufnr)
+        bnnoremap('<space>sn', '<cmd>lua vim.lsp.buf.rename()<CR>', 'Smart Rename', bufnr)
+        bnnoremap('K', '<cmd>lua vim.lsp.buf.hover()<CR>', 'Hover', bufnr)
+        bnnoremap('<leader>D', '<cmd>Telescope diagnostics bufnr=1<CR>', 'Telescope Diagnostics', bufnr) -- show  diagnostics for fil, bufnre
+        bnnoremap('<leader>d', '<cmd>vim.diagnostic.open_float<CR>', 'Show diagnostics for line', bufnr) -- show diagnostics for line
+        bnnoremap('<leader>ct', '<cmd>ClangdAST<CR>', 'Abstract Syntax Tree', bufnr)
+        bnnoremap('<leader>cu', '<cmd>ClangdMemoryUsage<CR>', 'Clangd Memory Usage', bufnr)
+        bnnoremap('<leader>k', '<cmd>ClangdSymbolInfo<CR>', 'ClangdSymbolInfo', bufnr)
+      end
+
+
+
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
       -- configure python server
@@ -56,34 +81,14 @@ return {
 
       -- configure c++ server
       lspconfig.clangd.setup {
-        on_attach = function(client, bufnr)
-          require("lsp-format").on_attach(client, bufnr)
-          require 'nvim-navic'.attach(client, bufnr)
-
-          -- clangd_extensions tools
-          require("clangd_extensions.inlay_hints").setup_autocmd()
-          require("clangd_extensions.inlay_hints").set_inlay_hints()
-
-          -- regular buffer maps
-          bnnoremap('K', '<cmd>lua vim.lsp.buf.hover()<CR>', 'Hover', bufnr)
-          vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>',
-            { desc = "Go definition", noreamp = true })
-          bnnoremap('<leader>i', '<cmd>lua vim.lsp.buf.signature_help()<CR>', 'Signature help', bufnr)
-          bnnoremap('<leader>tD', '<cmd>lua vim.lsp.buf.type_definition()<CR>', 'Type definition', bufnr)
-          bnnoremap('gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', 'Go to declaration', bufnr)
-          bnnoremap('<leader>a', '<cmd>lua vim.lsp.buf.code_action()<CR>', 'See code actions', bufnr)
-          bnnoremap('<space>sn', '<cmd>lua vim.lsp.buf.rename()<CR>', 'Smart Rename', bufnr)
-          bnnoremap('<leader>d', '<cmd>vim.diagnostic.open_float<CR>', 'Show diagnostics for line', bufnr) -- show diagnostics for line
-          bnnoremap('<leader>ct', '<cmd>ClangdAST<CR>', 'Abstract Syntax Tree', bufnr)
-          bnnoremap('<leader>cu', '<cmd>ClangdMemoryUsage<CR>', 'Clangd Memory Usage', bufnr)
-          bnnoremap('<leader>k', '<cmd>ClangdSymbolInfo<CR>', 'ClangdSymbolInfo', bufnr)
-        end,
+        capabilities = capabilities,
+        on_attach = clang_attach,
         cmd = {
           "clangd",
-          "--completion-style=detailed",
-          "--clang-tidy",
-          "--clang-tidy-checks=*",
-          "--cross-file-rename",
+          -- "--completion-style=detailed",
+          -- "--clang-tidy",
+          -- "--clang-tidy-checks=*",
+          -- "--cross-file-rename",
         }
       }
 
